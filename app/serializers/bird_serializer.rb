008cc552ad04a161720816f352cdd64e616a9ca6
Phil Billingsby <philbillingsby@gmail.com>
2020-05-14 10:49:11 -0400

    Done.

--- app/serializers/bird_serializer.rb
+++ app/serializers/bird_serializer.rb
@@ -4,4 +4,8 @@ class BirdSerializer
   # attributes :name_with_species do |obj| # Adds custom attribute
   #   "Name: #{obj.name}. Species: #{obj.species}."
   # end
+
+  attributes :link_to_species_image do |obj| # Creates a link to google images to bird
+    "https://www.google.com/search?q=" + obj.name + "&source=lnms&tbm=isch&sa=X"
+  end
 end
