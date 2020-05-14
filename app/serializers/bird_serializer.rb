6a8bb8043b2880c76b71f2baa3dcb3dd696dd5f2
Phil Billingsby <philbillingsby@gmail.com>
2020-05-14 10:46:10 -0400

    Done.

--- app/serializers/bird_serializer.rb
+++ app/serializers/bird_serializer.rb
@@ -1,4 +1,7 @@
 class BirdSerializer
   include FastJsonapi::ObjectSerializer
   attributes :name, :species
+  # attributes :name_with_species do |obj| # Adds custom attribute
+  #   "Name: #{obj.name}. Species: #{obj.species}."
+  # end
 end
