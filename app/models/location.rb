806e346b7c400ae814c091deee3085773289a28f
jayson_kesterson <your_email@example.com>
2020-05-17 13:13:20 -0400

    Done.

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,4 +1,6 @@
 class Location < ApplicationRecord
+    include FastJsonapi::ObjectSerializer
+    attributes :latitude, :longitude
     has_many :sightings
     has_many :birds, through: :sightings
 end
